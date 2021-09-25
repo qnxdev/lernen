a782f195cf39e8ab3d71c9f14da1569197854f65
qnxdev <qnxdev@outlook.com>
2021-09-25 15:07:53 +0530

    Minimify

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,7 @@
 import '../styles/globals.css'
+import '../styles/Home.css'
+
+/*Main App File - This is not a page*/
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,7 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
+//This is a Rest API it will run only on Serverside (Not a page)
+
 export default (req, res) => {
   res.status(200).json({ name: 'John Doe' })
 }
