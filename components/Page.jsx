568d5636cf66b9d7915555e47b03df1fadc35a4e
Home <home@pop-os.localdomain>
2021-10-19 11:48:35 +0530

    now updated

--- components/Page.jsx
+++ components/Page.jsx
@@ -4,9 +4,15 @@ export default function Page({ children }) {
   return (
     <div className="page">
       <header className="flex stretch">
-        <h1>L<span>e</span>rn<span>e</span>n</h1>
-        <a className="contact" href="">Contact Us</a>
-        <Button >Sign Up Now</Button>
+        <h1>
+          L<span>e</span>rn<span>e</span>n
+        </h1>
+        <div className="flex">
+          <a className="contact" href="">
+            Contact Us
+          </a>
+          <Button>Sign Up Now</Button>
+        </div>
       </header>
 
       <main className="main">{children}</main>
